6218111a29fd3b0db5c00e9466f79784c93f47f7
Piyush <piyushverma.809@gmail.com>
2024-06-05 23:45:59 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          color: Colors.blue, // Ensure the AppBar uses the primary color
-        ),
-      ),
-      home: HomePage(),
+      /// home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme:
+          ThemeData(brightness: Brightness.dark, primarySwatch: Colors.red),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => HomePage(),
+      },
     );
   }
 }
